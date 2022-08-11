2e83b1428f4c021eeadcfbc8c9fd5a3cb8774dee
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-11 19:06:00 +0300

    started our cmps tree

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 const HomePage = () => {
+  const featuredEvent = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home page</h1>
+      <ul></ul>
     </div>
   );
 };
