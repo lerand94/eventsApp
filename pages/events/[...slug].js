a40a6ab3c6f17dbf2f4d35a72057ab675c745c01
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-11 18:57:35 +0300

    created our pages( routes ) and add helper file dummy-data for testing

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>FilteredEvents page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>EventDetail page</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
