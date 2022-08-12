d4e5c361e779a9071e57d4089bda3b8c39d24177
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-12 15:31:32 +0300

    added event-detail cmps

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/EventDetail/EventSummary";
+import EventLogistics from "../../components/EventDetail/EventLogistics";
+import EventContent from "../../components/EventDetail/EventContent";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
-    <div>
-      <h1>EventDetail page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
