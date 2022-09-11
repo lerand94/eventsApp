0528ce5dbb1c94cbd2989d16c64146731f6fed80
Kirill <27140985+lerand94@users.noreply.github.com>
2022-09-11 17:02:06 +0300

    added mongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -7,7 +9,15 @@ const handler = (req, res) => {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.tfbt3tt.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: "Signed up!" });
   }
 };
